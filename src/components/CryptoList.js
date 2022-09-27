d294b547fdd663bfd1d7ef52d3337007844da934
Alejandro Jabo <ajabo@multitech.com.ar>
2022-09-27 17:44:48 -0300

    adding the first page

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,4 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function CryptoList(){
+
+    useEffect(()=>{
+
+        const endpoint = 'https://api.exchange.cryptomkt.com/api/3/public/currency';
+        console.log(endpoint)
+        axios.get(endpoint)
+        
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+
+    },[])
+
+
     return(
         <>
         <h1>CryptoList</h1>
